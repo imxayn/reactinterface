48c02ba3135719992d511e67f208f148b2855eba
imxayn <imxayn2000@gmail.com>
2019-08-22 10:49:06 +0400

    Updates

---
+++ precache-manifest.466c809aaea0361fa792f496378101ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c91110d3d3f619002854be4cc8b1b4fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dce4bc02a6b1c88a1d95",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "3378caa6a854d0a8b298",
+    "url": "/static/css/main.17b0b1c8.chunk.css"
+  },
+  {
+    "revision": "dce4bc02a6b1c88a1d95",
+    "url": "/static/js/2.8fd1256f.chunk.js"
+  },
+  {
+    "revision": "3378caa6a854d0a8b298",
+    "url": "/static/js/main.82fefb4b.chunk.js"
+  },
+  {
+    "revision": "2de6f60dcc199de330dd",
+    "url": "/static/js/runtime~main.70807e61.js"
+  }
+]);
